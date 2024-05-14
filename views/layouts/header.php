9c9c10b77095710c5075d320958d09a04589f8ff
Navvatart <146699264+Navvatart@users.noreply.github.com>
2024-05-14 21:59:35 +0700

    Update header.php

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -23,7 +23,7 @@
                 <div class="container-fluid">
                     <div>
                         <h2>GENPRO</h2>
-                        <input id="localversion" type="hidden" value="Beta-v02.05.14.2024">
+                        
                         <span class="tagline">Generate Proxies Openclash OpenWRT</span>
                         <div class="tombol tagline" id="deskripsi"></div>
                     </div>
