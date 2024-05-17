749a7278c4cb4000beaaeffc4425254061e672de
Navvatart <146699264+Navvatart@users.noreply.github.com>
2024-05-17 19:38:52 +0700

    remove form in cloudflared config

--- views/cloudflared.php
+++ views/cloudflared.php
@@ -21,14 +21,14 @@
   
 <div class="container">
   <div class="row">
-   <div class="mb-3">
+   <!-- <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Token</label>
         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
    </div>
    <div class="mb-3">
         <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-   </div>
+   </div> -->
    <div class="col-8"></div>
   <div class="container mt-3 col-4">
   <h2>Button Styles</h2>
